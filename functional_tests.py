ff6fa3dfc28ef0072e92b9192bd9abc8fa08b2bc
quantdaddy <surajkeshri@gmail.com>
2016-07-13 12:34:10 -0400

    Added POST capability but all the posted item doesn't display yet. Added two posts in functionaltest.py

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import time
 from selenium.webdriver.common.keys import Keys
 import unittest
 
@@ -14,15 +15,15 @@ class NewVisitorTest(unittest.TestCase):
 		self.browser.quit()
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
-		# Suraj goes to check out the homepage of our app
+		# Julia goes to check out the homepage of our app
 		self.browser.get('http://localhost:8000')
 
-		# He notices the page title and header mention to-do lists
+		# She notices the page title and header mention to-do lists
 		self.assertIn('To-Do', self.browser.title)
 		header_text = self.browser.find_element_by_tag_name('h1').text
 		self.assertIn('To-Do', header_text)
 
-		# He is invited to enter a to-do item straight away
+		# She is invited to enter a to-do item straight away
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
@@ -32,16 +33,22 @@ class NewVisitorTest(unittest.TestCase):
 		# When she hits enter, the page updates, and now the page lists 
 		# "1: Buy peacock feathers" as an item in a to-do list table
 		inputbox.send_keys(Keys.ENTER)
+		time.sleep(5)
+		# There is still a text box inviting her to add another item. She
+		# enters "Use peacock feathers to make a fly"
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(5)
 
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-				any(row.text == '1: Buy peacock feathers' for row in rows),
-				"New to-do item did not appear in table"
-			)
+		
 
-		# There is still a text box inviting her to add another item. She
-		# enters "Use peacock feathers to make a fly"
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+		
+
 		self.fail("Finish the test")
 
 if __name__ == '__main__':
