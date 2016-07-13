1a532ab247dd10d56b0e56de78fc6ca82e1e8cb3
quantdaddy <surajkeshri@gmail.com>
2016-07-13 17:03:46 -0400

    Added a functional test for another user checking if their lists are private

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 import time
 from selenium.webdriver.common.keys import Keys
-import unittest
+# import unittest
 from django.test import LiveServerTestCase
 
 class NewVisitorTest(LiveServerTestCase):
@@ -39,6 +39,8 @@ class NewVisitorTest(LiveServerTestCase):
 		# When she hits enter, the page updates, and now the page lists 
 		# "1: Buy peacock feathers" as an item in a to-do list table
 		inputbox.send_keys(Keys.ENTER)
+		julia_list_url = self.browser.current_url
+		self.assertRegex(julia_list_url, '/lists/.+')
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 		# There is still a text box inviting her to add another item. She
@@ -53,6 +55,33 @@ class NewVisitorTest(LiveServerTestCase):
 		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 		
 		
-		
+		# Now a new user, Francis, comes along to the site
 
-		self.fail("Finish the test")
+		## We use a new browser session to make sure that no information 
+		## of Julia's is coming along through cookies etc. 
+
+		self.browser.quit()
+		self.browser = webdriver.Chrome("/home/suraj/Downloads/chromedriver")
+
+		# Francis visits the home page. There is no sign of Julia's list
+		self.browser.get(self.live_server_url)
+		page_text = self.browser.find_element_by_id('body').text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertNotIn('make a fly', page_text)
+
+		# Francis start a new list by entering a new item. He is less interesting than Julia
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Buy milk')
+		inputbox.send_keys(Keys.ENTER)
+
+		# Francis gets his own unique URL
+		francis_list_url = self.browser.current_url
+		self.assertRegex(francis_list_url, '/lists/.+')
+		self.assertNotEqual(francis_list_url, julia_list_url)
+
+		# Again there is no trace of Julia's list
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertIn('Buy milk', page_text)
+
+		# Satisfies they both go back to drugs
