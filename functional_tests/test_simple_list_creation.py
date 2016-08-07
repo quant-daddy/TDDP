01564bc582c879dfc15aa9a36f5634694e30e9ef
quantdaddy <surajkeshri@gmail.com>
2016-08-07 08:02:49 -0400

    rename all the item input ids and names. FT still broken

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -21,7 +21,7 @@ class NewVisitorTest(FunctionalTest):
 		self.assertIn('To-Do', header_text)
 
 		# She is invited to enter a to-do item straight away
-		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox = self.get_item_input_box()
 		self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
 		# She types "buy peacock feathers" into a text box
@@ -36,7 +36,7 @@ class NewVisitorTest(FunctionalTest):
 
 		# There is still a text box inviting her to add another item. She
 		# enters "Use peacock feathers to make a fly"
-		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox = self.get_item_input_box()
 		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
 
@@ -61,7 +61,7 @@ class NewVisitorTest(FunctionalTest):
 		self.assertNotIn('make a fly', page_text)
 
 		# Francis start a new list by entering a new item. He is less interesting than Julia
-		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox = self.get_item_input_box()
 		inputbox.send_keys('Buy milk')
 		inputbox.send_keys(Keys.ENTER)
 
--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -3,7 +3,7 @@ from lists.forms import ItemForm, EMPTY_LIST_ERROR
 
 class ItemFormTest(TestCase):
 
-	def test_form_renders_item_text_input(self):
+	def test_form_renders_text_input(self):
 		form = ItemForm()
 		self.assertIn('placeholder="Enter a to-do item"', form.as_p())
 		self.assertIn('class="form-control input-lg"', form.as_p())
@@ -15,4 +15,3 @@ class ItemFormTest(TestCase):
 				form.errors['text'],
 				[EMPTY_LIST_ERROR]
 			)
-
